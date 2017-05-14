bebd0376258f323750760223adb1be3beade0900
Nilesh <nilesh.vidhate.prdxn@gmail.com>
2017-05-14 16:10:13 +0530

    title and description added for video

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -48,18 +48,20 @@ if(count($get_all_video)>0)
  		
 $this->validate($request, [
       'video' => 'required|mimes:mp4|max:25600',
+      'video_title'=>'required|max:100',
+      'video_description'=>'required|max:1000'
       ]); 		
  		 $imageName = time().'.'.$request->video->getClientOriginalExtension();
         $is_file_uploaded=$request->video->move(public_path('video'), $imageName);
 
 /* if $is_file_uploaded is true then save to table else return false */
 
-$class_id='1';
-
 if($is_file_uploaded){
 	$video=new Video;
 	$video->video_url=$imageName;
 	$video->class_id=$class_id;
+	$video->video_title=$request->video_title;
+	$video->video_description=$request->video_description;
 	$video->upload_by=Auth::user()->id;
 	$video->save();
 	$success=1;
